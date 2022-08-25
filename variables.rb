a49940435c7ab906cfa423c94ee4bce7bdd0a643
Leo Banos Garcia <bsleo625@gmail.com>
2022-08-24 21:08:35 -0600

    Added integer variables

--- variables.rb
+++ variables.rb
@@ -1,3 +1,7 @@
-Cake = "Tres Leches"
-Flavor = "Chocolate"
-Bakery = "King Soopers"
+cake = "Tres Leches"
+flavor = "Chocolate"
+bakery = "King Soopers"
+
+price_in_dollars = 25
+quantity = 10
+days_until_completion = 30
