cc77066b43d2a9d9eb2c1b830be8207ea18b5fea
Leo Banos Garcia <bsleo625@gmail.com>
2022-08-24 21:29:11 -0600

    Added puts statements

--- variables.rb
+++ variables.rb
@@ -13,3 +13,19 @@ elevation_in_feet = 3502.23
 tasty = true
 cheap = false
 hefty = true
+
+cake = "Berry Chantelle"
+flavor = "Strawberry"
+
+puts ""
+puts "My sister is so excited for the #{cake} to be delivered in #{days_until_completion} days!"
+puts ""
+puts "And,"
+puts weight_in_ounces * amount_of_cream_boops
+puts "is a value of some kind. Probably"
+puts ""
+puts "We live at #{elevation_in_feet}ft above sea level"
+puts ""
+puts "This, #{amount_of_cream_boops} plus #{elevation_in_feet} and #{weight_in_ounces} doesn't mean a whole lot."
+puts weight_in_ounces + amount_of_cream_boops + elevation_in_feet
+puts "See?"
